02d3c51894b8b67f3a1c98f2ef70a2def1376e23
Kaan <kaangokdemir01@gmail.com>
2020-08-10 21:41:59 +0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "no-unused-vars" : 1,
+    "comma-dangle": ["error", "always-multiline"],
+    "quotes": [2, "single"],
+    "prefer-template": ["error"],
+    "no-useless-concat": ["error"],
+    "prefer-destructuring": ["error", 
+      {
+        "object": true
+      }
+    ]
+  }
+}
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Toolkit } = require('actions-toolkit')
-const { execSync } = require('child_process')
 const bump = require('json-bump')
 
 // Change working directory if user defined PACKAGEJSON_DIR
@@ -15,7 +14,7 @@ Toolkit.run(async (tools) => {
   const commitMessage = 'version bump to v'
 
   try {
-    // set git user
+    // SET USER
     await tools.runInWorkspace('git', [
       'config',
       'user.name',
@@ -31,13 +30,19 @@ Toolkit.run(async (tools) => {
     ])
 
     const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(
-      process.env.GITHUB_REF
+      process.env.GITHUB_REF,
     )[1]
 
     await tools.runInWorkspace('git', ['checkout', currentBranch])
+
+    // BUMPING STARTS
     await bump(fileName)
 
-    let newVersion = JSON.parse(tools.getFile(fileName)).version
+    if (fileName === 'package.json') {
+      await bump('package.lock.json')
+    }
+
+    const newVersion = JSON.parse(tools.getFile(fileName)).version
 
     await tools.runInWorkspace('git', [
       'commit',
@@ -46,10 +51,8 @@ Toolkit.run(async (tools) => {
       `ci: ${commitMessage} ${newVersion}`,
     ])
 
-    console.log(newVersion)
-
+    // PUSH THE CHANGES
     const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
-    // console.log(Buffer.from(remoteRepo).toString('base64'))
     await tools.runInWorkspace('git', ['tag', newVersion])
     await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags'])
     await tools.runInWorkspace('git', ['push', remoteRepo, '--tags'])
