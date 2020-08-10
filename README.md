1b40267e6479da34afab53844771a05262051894
Kaan Gökdemir <kaangokdemir01@gmail.com>
2020-08-10 22:49:36 +0300

    V 0.1.0 (#1)
    * progress
    
    * branch
    
    * .
    
    * test
    
    * missed commit
    
    * up
    
    * ci: version bump to 0.0.13
    
    * imp
    
    * ci: version bump to v 0.0.14
    
    * eslint
    
    * package-lock
    
    * ci: version bump to v 0.0.15
    
    * test
    
    * ci: version bump to v 0.0.102
    
    * tag removed
    
    * ci: version bump to v 0.0.103
    
    * yarn support
    
    * package.json
    
    * ci: version bump to v 0.0.104
    
    * ci: version bump to v 0.0.16
    
    * focus
    
    * readme
    
    * yarn removed
    
    * ci: version bumped to v 0.0.17
    
    * v0.1.0
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
    Co-authored-by: GitHub Version Bumper <github-version-bumper@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-## Github Version Bumper
+# Github Version Bumper
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Bump your applications' version after each push.
+
+
+## Usage
+
+Create a `yaml` file in workflows folder.
+```bash
+touch .github/workflows/version.yml
+```
+
+And configure your settings
+```yaml
+name: "Bump Version"
+
+on:
+  push:
+    branches:
+      - "master"
+
+jobs:
+  bump-version:
+    name: "Bump Version on master"
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: "Checkout source code"
+        uses: "actions/checkout@v2"
+        with:
+          ref: ${{ github.ref }}
+      - name: "cat package.json"
+        run: cat ./package.json
+      - name: "Setup Node.js"
+        uses: "actions/setup-node@v1"
+        with:
+          node-version: 12
+      - name: "GitHub Version Bumper"
+        uses: "kaangokdemir/github-version-bumper@master"
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          GITHUB_NAME: "Your Name"
+          GITHUB_EMAIL: "your_email@gmail.com"
+          VERSION_FILE_NAME: "your_version_file.json"
+      - name: "cat package.json"
+        run: cat ./package.json
+```
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## TODO
+
+- Currently only supports `json` need to support `yaml` an `toml` files as well.
+- Currently only updates version key in json files. Make it customizable.
+- Currently only patches the version. Minor and major versions could be updatable as well.
+- Migrate to TypeScript.
+
+## Contributors
+
+Kaan Gökdemir - Author ([@kaangokdemir](https://twitter.com/kaangokdemir)) - [kaangokdemir.com](https://kaangokdemir.com)
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Toolkit } = require('actions-toolkit')
-const { execSync } = require('child_process')
 const bump = require('json-bump')
 
 // Change working directory if user defined PACKAGEJSON_DIR
@@ -11,76 +10,53 @@ if (process.env.PACKAGEJSON_DIR) {
 // Run your GitHub Action!
 Toolkit.run(async (tools) => {
   const fileName = process.env.VERSION_FILE_NAME || 'package.json'
-  const pkg = JSON.parse(tools.getFile(fileName))
-  console.log(pkg)
-  const event = tools.context.payload
 
-  if (!event.commits) {
-    console.log(
-      "Couldn't find any commits in this event, incrzementing patch version..."
-    )
-  }
-
-  const messages = event.commits
-    ? event.commits.map((commit) => commit.message + '\n' + commit.body)
-    : []
-
-  const commitMessage = 'version bump to'
-  const isVersionBump = messages
-    .map((message) => message.toLowerCase().includes(commitMessage))
-    .includes(true)
-  if (isVersionBump) {
-    tools.exit.success('No action necessary!')
-    return
-  }
-
-  let version = 'patch'
-  if (
-    messages
-      .map(
-        (message) =>
-          message.includes('BREAKING CHANGE') || message.includes('major')
-      )
-      .includes(true)
-  ) {
-    version = 'major'
-  } else if (
-    messages
-      .map(
-        (message) =>
-          message.toLowerCase().startsWith('feat') ||
-          message.toLowerCase().includes('minor')
-      )
-      .includes(true)
-  ) {
-    version = 'minor'
-  }
+  const commitMessage = 'version bumped to v'
 
   try {
-    // set git user
+    // SET USER
     await tools.runInWorkspace('git', [
       'config',
       'user.name',
-      `"${process.env.GITHUB_USER || 'Automated Version Bump'}"`,
+      `"${process.env.GITHUB_USER || 'GitHub Version Bumper'}"`,
     ])
     await tools.runInWorkspace('git', [
       'config',
       'user.email',
       `"${
         process.env.GITHUB_EMAIL ||
-        'gh-action-bump-version@users.noreply.github.com'
+        'github-version-bumper@users.noreply.github.com'
       }"`,
     ])
 
     const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(
-      process.env.GITHUB_REF
+      process.env.GITHUB_REF,
     )[1]
 
     await tools.runInWorkspace('git', ['checkout', currentBranch])
+
+    // BUMPING STARTS
     await bump(fileName)
 
+    if (fileName === 'package.json') {
+      try {
+        await bump('package-lock.json')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const newVersion = JSON.parse(tools.getFile(fileName)).version
+
+    await tools.runInWorkspace('git', [
+      'commit',
+      '-a',
+      '-m',
+      `ci: ${commitMessage} ${newVersion}`,
+    ])
+
+    // PUSH THE CHANGES
     const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
-    // console.log(Buffer.from(remoteRepo).toString('base64'))
     await tools.runInWorkspace('git', ['tag', newVersion])
     await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags'])
     await tools.runInWorkspace('git', ['push', remoteRepo, '--tags'])
