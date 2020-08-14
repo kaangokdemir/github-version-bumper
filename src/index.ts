fa05d8ed5e051e63e273407cccbe7eed29006245
Kaan <kaangokdemir01@gmail.com>
2020-08-14 20:06:38 +0300

    [ci-bump version="0.3.77-saturn"] pluto

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,10 @@ Toolkit.run(async (tools) => {
 
     if (lastCommit.includes('[ci-bump version=')) {
       const splitted = lastCommit.split('[ci-bump version=\\"')
+
+      console.log('splitted', splitted)
+      console.log('splitted-0', splitted[0])
+      console.log('splitted-1', splitted[1])
       const version = splitted[1].split('\\"')[0]
       await bumpVersion(fileName, { version })
     } else if (lastCommit.includes('[ci-bump pre=')) {
